a9b43bfc82cfb841bd151cedb03e42c8c7c7befa
unknown <admin@DESKTOP-7BMH73T.officesg.local>
2026-04-13 14:57:04 +0700

    Auto Update: Mon 04/13/2026 14:57:04.73

--- push.bat
+++ push.bat
@@ -1,26 +1,7 @@
-@echo off
-chcp 65001 >nul
-echo ----------------------------------------------------
-echo         ĐẨY CODE MỚI LÊN GITHUB & RENDER
-echo ----------------------------------------------------
-
-echo [1/3] Đang lấy tất cả file vừa thay đổi...
 git add .
-
-echo.
-set /p message="[2/3] Nhập ghi chú commit (Nhấn Enter để dùng mặc định 'Cập nhật nội dung'): "
-if "%message%"=="" set message=Cập nhật nội dung
-
-git commit -m "%message%"
-
-echo.
-echo [3/3] Đang đẩy code lên Github...
+git commit -m "Auto Update: %date% %time%"
 git push
-
-echo.
-echo ====================================================
-echo HOÀN TẤT THÀNH CÔNG!
-echo Code đã nằm trên Github. Render.com sẽ bắt đầu build tự động.
-echo Bạn vui lòng chờ khoảng 2-3 phút trước khi gọi lại API trên n8n nhé!
-echo ====================================================
+echo =====================
+echo HOAN TAT (DONE)
+echo =====================
 pause
